113e1b331667e6b7636f53e1e3f8e720cf7b83f4
PriscaJ <joseph.pr@husky.neu.edu>
2018-03-18 02:10:04 -0400

    text view and beginning of visual view

--- src/cs3500/animator/view/AnimationPanel.java
+++ src/cs3500/animator/view/AnimationPanel.java
@@ -1,4 +1,38 @@
 package cs3500.animator.view;
 
-public class AnimationPanel {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import cs3500.animator.controller.Controller;
+import cs3500.animator.model.IReadOnlyModel;
+
+
+/**
+ * This panel represents the area where the shapes and actions of an animation will be displayed.
+ */
+public class AnimationPanel extends JPanel implements ActionListener {
+  // todo have a controller actionlistener to include in the constructor???
+  private int tick;
+  private Timer t;
+
+  public AnimationPanel() {
+    this.setBackground(Color.WHITE);
+    this.tick = -1;
+    this.t = new Timer(0, this);
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
 }
