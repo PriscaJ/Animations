5ad67f9b3261f7ccc4e47130471e651cf022bfdd
PriscaJ <joseph.pr@husky.neu.edu>
2018-04-16 16:00:55 -0400

    extends and implements in adapters

--- src/cs3500/animator/model/ModelAdapter.java
+++ src/cs3500/animator/model/ModelAdapter.java
@@ -6,41 +6,46 @@ import cs3500.animator.provider.model.Command;
 import cs3500.animator.provider.model.Shape;
 import cs3500.animator.provider.model.SimpleAnimation;
 
-public class ModelAdapter implements SimpleAnimation{
+public class ModelAdapter extends AnimationModel implements SimpleAnimation{
   AnimationOperations oldModel;
+  AnimationModel concreteModel;
 
   public ModelAdapter(AnimationOperations oldModel) {
     this.oldModel = oldModel;
+    concreteModel = new AnimationModel();
   }
 
 
   @Override
   public void animate() throws IllegalArgumentException {
-
+    animate();
   }
 
   @Override
   public String printAnimation() {
-    return null;
+    // moved into the textual view and taken out of the model
+    return "";
   }
 
   @Override
   public void addShape(Shape shape) throws IllegalArgumentException {
-
+    addShape(shape);
   }
 
   @Override
   public void addCommand(Command command, Shape s) throws IllegalArgumentException {
-
+    addCommand(command, s);
   }
 
   @Override
   public Shape getShapeWithName(String name) {
+    // todo need to access the hashmap in the concrete model
     return null;
   }
 
   @Override
   public List<Shape> getShapes() {
-    return null;
+    // todo conflicting shapes
+    return (List) oldModel.getShapes();
   }
 }
